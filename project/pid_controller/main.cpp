effaefb596762aa23c326c2e7c47c2792cb0e5c4
Marcelo Silva <marcelo.silva@foretellix.com>
2025-02-21 20:48:57 -0300

    a

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -226,11 +226,11 @@ int main ()
   **/
 
   PID pid_steer = PID();
-  double steer_kp = 4.0;
+  double steer_kp = 2.0;
   double steer_ki = 0.1;
   double steer_kd = 0.0;
-  double steer_out_max = 0.5236;    // 0.5236 rad = 30 degrees (30/180*pi)
-  double steer_out_min = -0.5236;   // 0.5236 rad = 30 degrees (30/180*pi)
+  double steer_out_max = 0.5236/2.0;    // 0.5236 rad = 30 degrees (30/180*pi)
+  double steer_out_min = -0.5236/2.0;   // 0.5236 rad = 30 degrees (30/180*pi)
   pid_steer.Init(steer_kp, steer_ki, steer_kd, steer_out_max, steer_out_min);
 
   double throtlle_kp = 0.2;
